50eeff6b0354258ca90ea43aaf3bb3bd7a14ab9c
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2023-07-22 09:28:02 -0500

    fin seccion 9

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
+
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: [
+  ]
+})
+export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private route: Router,
+    private countriesServices: CountriesService,
+  ) { }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesServices.searchCountryByAlphaCode(id))
+      )
+      .subscribe(country => {
+        if (!country) return this.route.navigateByUrl('');
+        return this.country = country;
+      })
+  }
+}
